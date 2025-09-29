a401459829d5300986d3158902a850c251a6c46f
Ben Lynch <G00403364@atu.ie>
2025-09-29 09:25:23 +0100

    Initial Commit

---
+++ src/main/java/ie/atu/week3/MainApp.java
@@ -0,0 +1,16 @@
+package ie.atu.week3;
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        Scanner scan1 = new Scanner(System.in);
+
+        System.out.println("Please enter a number:");
+        double firstNum = scan1.nextDouble();
+        System.out.println("Please enter a second number:");
+        double secondNum = scan1.nextDouble();
+
+        
+    }
+}
