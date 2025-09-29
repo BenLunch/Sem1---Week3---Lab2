0e063d8dce588eb73d81f69cde29035011788434
Ben Lynch <G00403364@atu.ie>
2025-09-29 10:01:51 +0100

    Commit addition and main app format

--- src/main/java/ie/atu/week3/Calculator.java
+++ src/main/java/ie/atu/week3/Calculator.java
@@ -2,4 +2,9 @@ package ie.atu.week3;
 
 public class Calculator {
 
+    public double add(double firstNum, double secondNum) {
+        double result = firstNum + secondNum;
+        return result;
+    }
+
 }
